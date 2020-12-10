28c16c11bd278e66bc29bedb5cb9ad87769beaf8
Laura Sach <laura.sach@raspberrypi.org>
2020-12-10 15:26:44 +0000

    Add day 9 part 2

--- Day 9/day9-part2.py
+++ Day 9/day9-part2.py
@@ -1,3 +1,5 @@
+import copy
+
 with open("input.txt") as f:
     data = f.readlines()
     data = [int(n.strip()) for n in data]
@@ -5,27 +7,42 @@ with open("input.txt") as f:
 # Define the search area
 start = 0
 end = 25
-current = 25    # The number we're looking at
 target = 104054607  # The number from part 1
 
-# Check whether the current number can be made up of any of the previous numbers added together
-def check_number(start, end, current, data):
-    search_list = data[start:end]
-    find = data[current]
-    for first in search_list:
-        for ind, second in enumerate(search_list):
-            if first + second == find:
-                return True
-    return False
+# First get rid of any numbers that are bigger than the target
+smaller_data = [n for n in data if n < target]
 
+# Reverse the list to look at the bigger numbers first
+smaller_data.reverse()
 
+for i in range(len(smaller_data)):
 
-while current < len(data):
-    if check_number(start, end, current, data):    
-        current += 1
-        start += 1
-        end += 1
+    data_for_loop = copy.deepcopy(smaller_data)
+    
+    # Look at each number and add it together with the remaining list
+    current = data_for_loop[i]
+    end = len(data_for_loop)
+    rest = data_for_loop[i+1:end]
+
+    found_it = False
+
+    while end > i+1:
+        #print("Trying ", str(rest))
+
+        if sum(rest) == target:
+            found_it = True
+            rest.sort()
+            print("I made ", target, " using ", str(rest))
+            print("The sum of the start and end was " + str(rest[0] + rest[len(rest)-1]))
+            break
+        else:
+            end = end - 1
+            rest = data_for_loop[i+1:end]
+        
+    if not found_it:
+        print("Didn't find it for i ", i)
     else:
-        print("Number ", data[current], " has no sums")
         break
+    
 
+# 11828200 too low
